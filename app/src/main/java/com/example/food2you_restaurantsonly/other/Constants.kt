084eccaf753518e419125cdaadae6f3f7b1e0b8d
Lubomir1998 <lubcho_sk8@abv.bg>
2021-01-25 19:38:10 +0200

    Push notification works... need some fixes

--- app/src/main/java/com/example/food2you_restaurantsonly/other/Constants.kt
+++ app/src/main/java/com/example/food2you_restaurantsonly/other/Constants.kt
@@ -1,5 +1,7 @@
 package com.example.food2you_restaurantsonly.other
 
+import com.example.food2you_restaurantsonly.BuildConfig
+
 object Constants {
 
     const val ENCRYPTED_SHARED_PREFS_NAME = "encrypted_shared_prefs"
@@ -10,7 +12,7 @@ object Constants {
 
     const val KEY_TOKEN = "KEY_TOKEN"
 
-    const val SERVER_KEY = "AAAAyp6DrtY:APA91bHUC1R7gss5Ob_fDQvP25vd_qRk9cGrd8y3EhQvptzjYJToxNuFALSlKy0_XmybV1nhLmwXT9_AJOmIgcC46OUwzpISm7CmJneORZMq3iGZORka8jv8rRTktr0kdsjTqWaKgIx-"
+    const val SERVER_KEY = BuildConfig.SERVER_KEY
     const val CONTENT_TYPE = "application/json"
 
 }
